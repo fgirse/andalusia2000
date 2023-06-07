b45d2407be6bc4d8d3c2b92187a60388f322bed1
fgirse <fgirse@bluewin.ch>
2023-06-07 15:59:22 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Feed from "@components/Feed";
 
 const Home = () => (
-  <section className='w-full flex-center flex-col'>
+  <section id="bghero" className='w-full flex-center flex-col'>
     <h1 className='head_text text-center'>
       
       <br className='max-md:hidden' />
