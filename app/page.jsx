d861a28804a5c61ba0aea030bd40c3edd5b97cde
fgirse <fgirse@bluewin.ch>
2023-06-08 11:41:40 +0200

    hero aLS BG-IMAE

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ const Home = () => (
  
  <>
   
- <section className='bg-[url("assets/images/andalusiaHero.jpg")] bg-cover bg-non-repeatw-full flex-center flex-col'>
+ <section className='bg-[url("assets/images/andalusiaHero.png")] bg-cover bg-non-repeatw-full flex-center flex-col'>
   <div className='bg-black/20 rounded-xl'>
     <h1 className='head_text text-center'>
       
