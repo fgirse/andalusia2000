a7de51df28d55605e3a14ce6f3c6bf691473d455
fgirse <fgirse@bluewin.ch>
2023-06-08 11:24:05 +0200

    logo wohndefuehl

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,24 @@
 import Feed from "@components/Feed";
 
 const Home = () => (
-  <section id="bghero" className='w-full flex-center flex-col'>
+ 
+ <>
+  
+ <section className='w-full flex-center flex-col'>
+  <div className='bg-black/20 rounded-xl'>
     <h1 className='head_text text-center'>
       
       <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> Andalusien kennen  - Andalusien lieben ♥   </span>
+      <span className='orange_gradient text-center'> Andalusien  </span>
     </h1>
     <p className='desc text-center'>
   Lebe Deinen Wohntraum !!!
     </p>
 
-    <Feed />
+    </div>
   </section>
+  <Feed />
+  </>
 );
 
 export default Home;
